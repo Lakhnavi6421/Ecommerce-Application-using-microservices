6c6c0771cc8bc89da84fe6a561a15451e51c8994
Pankaj Lakhnavi <pankajlakhnavi2001@gmail.com>
2026-05-14 20:31:23 +0530

    endpoints to fetching cart

--- src/main/java/com/app/ecom/controller/CartController.java
+++ src/main/java/com/app/ecom/controller/CartController.java
@@ -2,11 +2,15 @@ package com.app.ecom.controller;
 
 
 import com.app.ecom.dto.CartItemRequest;
+import com.app.ecom.model.CartItem;
+import com.app.ecom.model.Product;
 import com.app.ecom.service.CartService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
@@ -24,6 +28,23 @@ public class CartController {
             return ResponseEntity.badRequest().body("Product Out of Stock or User not found or Product not found.");
         }
         return ResponseEntity.status(HttpStatus.CREATED).build();
+    }
+
+    @DeleteMapping("/items/{productId}")
+    public ResponseEntity<Void> removeFromCart(
+            @RequestHeader("X-User-ID") String userId,
+            @PathVariable Long productId){
+
+        boolean deleted = cartService.deleteItemFromCart(userId, productId);
+        return deleted ? ResponseEntity.noContent().build() :
+                            ResponseEntity.notFound().build();
+    }
+
+    @GetMapping
+    public ResponseEntity<List<CartItem>> getCart(
+            @RequestHeader("X-User-ID") String userId){
+
+        return ResponseEntity.ok(cartService.getCart(userId));
     }
 
 }
