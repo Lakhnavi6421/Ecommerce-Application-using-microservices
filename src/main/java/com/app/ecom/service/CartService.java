562a5efce6b72a94dc5a15d025f3adae2357a3d4
Pankaj Lakhnavi <pankajlakhnavi2001@gmail.com>
2026-05-14 09:37:07 +0530

    User cart added

---
+++ src/main/java/com/app/ecom/service/CartService.java
@@ -0,0 +1,65 @@
+package com.app.ecom.service;
+
+import com.app.ecom.dto.CartItemRequest;
+import com.app.ecom.model.CartItem;
+import com.app.ecom.model.Product;
+import com.app.ecom.model.User;
+import com.app.ecom.repository.CartItemRepository;
+import com.app.ecom.repository.ProductRepository;
+import com.app.ecom.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.math.BigDecimal;
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
+public class CartService {
+
+    private final ProductRepository productRepository;
+
+    private final CartItemRepository cartItemRepository;
+
+    private final UserRepository userRepository;
+
+    public boolean addToCart(String userId, CartItemRequest request) {
+        // Step 1: Look for product
+
+        Optional<Product> productOpt = productRepository.findById(request.getProductId());
+        if(productOpt.isEmpty())
+            return false;
+
+        // Step 2: if quantity exist
+        Product product = productOpt.get();
+        if(product.getStockQuantity() < request.getQuantity())
+            return false;
+
+        // Step 3: if user exist
+        Optional<User> userOpt = userRepository.findById(Long.valueOf(userId));
+        if(userOpt.isEmpty())
+            return false;
+
+        User user = userOpt.get();
+
+        CartItem existingCartItem = cartItemRepository.findByUserAndProduct(user, product);
+
+        if(existingCartItem != null){
+            // Update the quantity
+            existingCartItem.setQuantity(existingCartItem.getQuantity() + 1);
+            existingCartItem.setPrice(product.getPrice().multiply(BigDecimal.valueOf(existingCartItem.getQuantity())));
+            cartItemRepository.save(existingCartItem);
+
+        }
+        else{
+            // Create new cart item
+            CartItem cartItem = new CartItem();
+            cartItem.setUser(user);
+            cartItem.setProduct(product);
+            cartItem.setQuantity(request.getQuantity());
+            cartItem.setPrice(product.getPrice().multiply(BigDecimal.valueOf(request.getQuantity())));
+            cartItemRepository.save(cartItem);
+        }
+        return true;
+    }
+}
